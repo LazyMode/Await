722fc2b50c8da040e6877cabc311f5160f5bc19f
Licshee <licshee@yen.ai>
2016-01-08 15:41:53 +0800

    Just await int & TimeSpan for Delay

---
+++ Await/Await.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+
+#if !Async40
+using TaskEx = System.Threading.Tasks.Task;
+#endif
+
+public static class Await
+{
+    public static TaskAwaiter GetAwaiter(this int ms)
+        => TaskEx.Delay(ms).GetAwaiter();
+    public static TaskAwaiter GetAwaiter(this TimeSpan ts)
+        => TaskEx.Delay(ts).GetAwaiter();
+}
