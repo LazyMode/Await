f1e99719ebb1ff77c93e41ee228f7ecc9401b7ae
Licshee <licshee@yen.ai>
2016-02-03 17:34:57 +0800

    public static TaskAwaiter GetAwaiter(this WaitHandle handle)

---
+++ Await/WaitHandle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+#if TaskEx
+using Microsoft.Runtime.CompilerServices;
+using static System.Threading.Tasks.TaskEx;
+#else
+using System.Runtime.CompilerServices;
+using static System.Threading.Tasks.Task;
+#endif
+
+static partial class Await
+{
+    public static TaskAwaiter GetAwaiter(this WaitHandle handle)
+        => Run(handle.WaitOne).GetAwaiter();
+}
