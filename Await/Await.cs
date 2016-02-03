ccaae260ec608e89279a8aeeb7d6f4e781d67100
Licshee <licshee@yen.ai>
2016-02-03 17:11:27 +0800

    #if TaskEx

--- Await/Await.cs
+++ Await/Await.cs
@@ -1,18 +1,18 @@
 ﻿using System;
 using System.Threading.Tasks;
 
-#if NET45 || NativeAwait || !(NET40 || LegacyAwait || CompatibleAwait)
-using System.Runtime.CompilerServices;
-using TaskEx = System.Threading.Tasks.Task;
-#else
+#if TaskEx
 using Microsoft.Runtime.CompilerServices;
-using TaskEx = System.Threading.Tasks.TaskEx;
+using static System.Threading.Tasks.TaskEx;
+#else
+using System.Runtime.CompilerServices;
+using static System.Threading.Tasks.Task;
 #endif
 
 static partial class Await
 {
     public static TaskAwaiter GetAwaiter(this int ms)
-        => TaskEx.Delay(ms).GetAwaiter();
+        => Delay(ms).GetAwaiter();
     public static TaskAwaiter GetAwaiter(this TimeSpan ts)
-        => TaskEx.Delay(ts).GetAwaiter();
+        => Delay(ts).GetAwaiter();
 }
